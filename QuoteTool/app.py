e28901dd208334277d7933855ea3a368703e6faf
Nick Stanzione <nick.stanzione@coherent.global>
2023-07-31 21:30:20 -0400

    Trying to debug QuoteTool

--- QuoteTool/app.py
+++ QuoteTool/app.py
@@ -17,22 +17,21 @@ file = st.file_uploader("Drop your census file here to load", type={"csv"})
 try:
     inputs = pd.read_csv(file)
     st.text("Upload success!")
+    req = to_json(inputs, request_meta)
+    st.text(req)
+
 except ValueError:
     st.text("Waiting for file...")
 
 
 #Run Batch
 @st.cache(suppress_st_warning=True)
-def get_data(x, rm, url, headers): #(x, rm, url, headers, threads)
-    req = to_json(x, rm)
-    # st.write(req)
-    #store = threaded_process_range(req, url, headers, threads, range(threads))
+def get_data(req, url, headers): #(x, rm, url, headers, threads)
     store = asyncio.run(async_batch_call(url, headers, req))
-    # st.write(store)
     z = write_results(store)
     return z
 
-res = get_data(inputs, request_meta, url, headers)
+res = get_data(req, url, headers)
 res_df = pd.json_normalize(res)
 premium = res_df['Premium'].sum()
 st.subheader('Annual Premium: :blue[${:0,.0f}]'.format(premium).replace('$-','-$'))
