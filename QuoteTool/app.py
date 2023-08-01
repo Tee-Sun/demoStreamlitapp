03fe99c75f7335da750f26745782d4bcbdf54d1f
Nick Stanzione <nick.stanzione@coherent.global>
2023-08-01 11:08:41 -0400

    Adding button and removing cache

--- QuoteTool/app.py
+++ QuoteTool/app.py
@@ -7,7 +7,6 @@ from ref import *
 
 # Initiate Data App
 st.title("Plan Quoting Tool")
-
 st.markdown('')
 
 #Set-up drag & drop input for csv upload into dataframe
@@ -18,26 +17,30 @@ try:
     inputs = pd.read_csv(file)
     st.text("Upload success!")
     req = to_request(inputs, request_meta)
-
 except ValueError:
     st.text("Waiting for file...")
 
-
-#Run Batch
-@st.cache(suppress_st_warning=True)
-def get_data(req, url, headers): #(x, rm, url, headers, threads)
+#Function to Run Batch
+# @st.cache(suppress_st_warning=True)
+def get_data(req, url, headers): 
     store = asyncio.run(async_batch_call(url, headers, req))
     z = write_results(store)
     return z
 
+#Set up Calculate button to control making API Call
 try:
-    res = get_data(req, url, headers)
-    res_df = pd.json_normalize(res)
-    premium = res_df['Premium'].sum()
-    st.subheader('Annual Premium: :blue[${:0,.0f}]'.format(premium).replace('$-','-$'))
-
+    if st.button('Calculate Premium'):
+        res = get_data(req, url, headers)
 except:
     pass
 
+#Return premium from API Call response if avaiable
+try:
+    # res = get_data(req, url, headers)
+    res_df = pd.json_normalize(res)
+    premium = res_df['Premium'].sum()
+    st.subheader('Annual Premium: :blue[${:0,.0f}]'.format(premium).replace('$-','-$'))
+except:
+    pass
 
 
